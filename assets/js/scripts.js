b88351a10f9d66c7cfbbcb8aa46d97b57e9882f5
Caio Régis <caio.regis@d0007azurecloudtotvscom.onmicrosoft.com>
2022-06-20 21:21:58 -0300

    chore(game-over): rules and config

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,4 +9,31 @@ const jump = () => {
     }, 500);
 }
 
+const isGameOver = (pipePosition, marioPosition) => {
+    return pipePosition <= 100 && pipePosition > 0 && marioPosition < 102;
+}
+
+const setGameOver = (pipePosition, marioPosition) => {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+        
+    mario.style.animation = 'none';
+    mario.src = './assets/images/game-over.png';
+    mario.style.width = '55px';
+    mario.style.bottom = `${marioPosition}px`;
+    mario.style.left = `20px`;
+}
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (isGameOver(pipePosition, marioPosition)) {
+        setGameOver(pipePosition, marioPosition);
+        
+        clearInterval(loop);
+    }
+
+}, 10);
+
 document.addEventListener('keydown', jump);
