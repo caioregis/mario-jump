12ee6d1b29aa655b089ffeba95c1684a692be191
Caio Régis <caio.regis@d0007azurecloudtotvscom.onmicrosoft.com>
2022-06-20 22:37:34 -0300

    chore(game-over): reset start game

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const startGameButton = document.querySelector('.start-game');
 
 const jump = () => {
     mario.classList.add('mario-jump');
@@ -14,26 +15,48 @@ const isGameOver = (pipePosition, marioPosition) => {
 }
 
 const setGameOver = (pipePosition, marioPosition) => {
-    pipe.style.animation = 'none';
+    pipe.classList.remove('pipe-slide');
     pipe.style.left = `${pipePosition}px`;
-        
-    mario.style.animation = 'none';
+    
+    mario.classList.remove('mario-jump');
     mario.src = './assets/images/game-over.png';
     mario.style.width = '55px';
     mario.style.bottom = `${marioPosition}px`;
-    mario.style.left = `20px`;
+    mario.style.left = '20px';
 }
 
-const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
-    if (isGameOver(pipePosition, marioPosition)) {
-        setGameOver(pipePosition, marioPosition);
+const resetGame = () => {
+    pipe.style.left = 'unset';
+    pipe.classList.add('pipe-slide');
         
-        clearInterval(loop);
-    }
+    mario.src = './assets/images/mario.gif';
+    mario.style.width = '100px';
+    mario.style.bottom = '0';
+    mario.style.left = '0';
+}
 
-}, 10);
+const start = () => {
+    resetGame();
 
-document.addEventListener('keydown', jump);
+    document.addEventListener('keydown', jump);
+    startGameButton.removeEventListener('click', start);
+    startGameButton.style.display = 'none';
+
+    const loop = setInterval(() => {
+
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+        if (isGameOver(pipePosition, marioPosition)) {
+            setGameOver(pipePosition, marioPosition);
+            
+            document.removeEventListener('keydown', jump);
+            startGameButton.style.display = 'block';
+            startGameButton.addEventListener('click', start);
+            clearInterval(loop);
+        }
+
+    }, 10);
+}
+
+start();
